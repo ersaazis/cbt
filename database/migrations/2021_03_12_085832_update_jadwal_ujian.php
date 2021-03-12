a2f9777498da955e972ec5a28e7581bc7248ee28
ersaazis <eam24maret@gmail.com>
2021-03-12 09:16:45 +0700

    update unique

--- database/migrations/2021_03_12_085832_update_jadwal_ujian.php
+++ database/migrations/2021_03_12_085832_update_jadwal_ujian.php
@@ -13,12 +13,14 @@ class UpdateJadwalUjian extends Migration
      */
     public function up()
     {
-        Schema::disableForeignKeyConstraints();
         Schema::table('jadwal_ujian', function (Blueprint $table) {
+            $table->dropForeign('jadwal_ujian_mapel_id_foreign');
+            $table->dropForeign('jadwal_ujian_paket_ujian_id_foreign');
             $table->dropUnique('jadwal_ujian_mapel_id_unique');
             $table->dropUnique('jadwal_ujian_paket_ujian_id_unique');
+            $table->foreign('mapel_id')->references('id')->on('mapel')->onDelete('cascade');
+            $table->foreign('paket_ujian_id')->references('id')->on('paket_ujian')->onDelete('cascade');
         });
-        Schema::enableForeignKeyConstraints();
     }
 
     /**
